779613e5e446eaa7395a1f34dc40d4cde0f20c5f
E214977W <mathieu.marthy@etu.univ-nantes.fr>
2023-03-21 17:35:35 +0100

    server: template server

--- server/dao/userDao.mjs
+++ server/dao/userDao.mjs
@@ -1,30 +1,8 @@
-import { MongoClient } from "mongodb";
+const {MongoClient} = require('mongodb');
 
-const url = "mongodb+srv://mathieumarthy:gmiCyUDf7I1IZz6d3jhtkUa4NqjQYksJWB0BUZl3cLD1JKjrJdG@sae.7grcpoy.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient("mongodb+srv://mathieumarthy:gmiCyUDf7I1IZz6d3jhtkUa4NqjQYksJWB0BUZl3cLD1JKjrJdG@sae.7grcpoy.mongodb.net/?retryWrites=true&w=majority");
 
-class UserDao {
-  constructor(uri) {
-    this.uri = uri;
-    this.client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    this.dbName = "sae";
-    this.collectionName = "users";
-
-    this.connect();
-  }
-
-  async connect() {
-    await this.client.connect();
-    this.collection = this.client.db(this.dbName).collection(this.collectionName);
-  }
-
-  async getAllUser() {
-    if (!this.collection) {
-      await this.connect();
-    }
-
-    const users = await this.collection.find({}).toArray();
-    return users;
-  }
+async function main() {
+    await client.connect();
+    console.log("Connected to MongoDB!");
 }
-
-export const userDao = new UserDao(url);
--- server/index.mjs
+++
@@ -1,4 +0,0 @@
-import { userDao } from "./dao/userDao.mjs";
-
-const users = await userDao.getAllUser();
-console.log(users);
