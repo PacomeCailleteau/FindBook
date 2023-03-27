da8657246cd44242ae2d442916adf1951038ff58
E212406B <pacome.cailleteau@etu.univ-nantes.fr>
2023-03-27 12:21:21 +0200

    page d'accueil bien avancée + book

---
+++ android/app/src/main/java/com/example/books_android/Books.kt
@@ -0,0 +1,17 @@
+package com.example.books_android
+
+class Books {
+    companion object {
+
+        val COMPARATOR = Comparator<Book> { t0, t1 -> t0.compareTo(t1) }
+        val COMPARATOR_ALPHABETICAL = Comparator<Book> { t0, t1 -> t0.title.compareTo(t1.title) }
+
+        fun initBookslist(): MutableList<Book> {
+            return mutableListOf<Book>(
+                Book("Le chat","monimage.png","ab0123456789","moi","10/12/2022","Fantastique","Le chat marche et tombe"),
+                Book("Le chien","monimage.png","ab0123456798","moi","11/12/2022","Fantastique","Le chien marche et tombe"),
+                Book("Le renard","monimage.png","ab0123452587","moi","12/12/2022","Fantastique","Le renard marche et tombe")
+                )
+        }
+    }
+}
