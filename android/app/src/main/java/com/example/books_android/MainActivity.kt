a0886070676b629a276d63549e83d625939e38c4
E215035J <H9Abo8nPw*8Rq9>
2023-03-28 12:22:08 +0200

    sa grand mere je vais tout peter

--- android/app/src/main/java/com/example/books_android/MainActivity.kt
+++ android/app/src/main/java/com/example/books_android/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.books_android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btnParam = findViewById<ImageButton>(R.id.btnParametre)
+        val btnConnexion = findViewById<ImageButton>(R.id.btnConnexion)
+
+
+        btnConnexion.setOnClickListener {
+            val connexion = Intent(this@MainActivity, ConnexionActivity::class.java)
+            startActivity(connexion)
+        }
     }
+
+
+
+
 }
