6db9577095277004724e2223ce00efc66e12790b
Nicolas Chusseau <nicolas.chusseau@etu.univ-nantes.fr>
2023-04-02 19:43:37 +0200

    début test pas testé

--- server/tests/test.mjs
+++ server/tests/test.mjs
@@ -5,6 +5,7 @@ import chai from "chai"
 import chaiHttp from "chai-http"
 
 let should = chai.should()
+let tok = ""
 
 chai.use(chaiHttp)
 
@@ -20,7 +21,49 @@ describe("user", () => {
         await server.stop();
     });
 
-    it("test")
+    it("créé un nouvel utilisateur", async () => {
+        const res = await server.inject({
+            method: "post",
+            url: "localhost:3001/users/create/test/pass",
+            payload: {
+                login: "test",
+                password: "pass"
+            }
+        })
+        chai.expect(res.statusCode).to.equal(201)
+        chai.expect(res.result.password).to.equal('pass');
+        chai.expect(res.result.login).to.eql('test')
+        //TODO : vérifier que le token est bien un str et qu'il existe
+        //on ne peut pas vérifier autre chose car il est fait aléatoirement
+        chai.expect(res.result.token).to.eql('test')
+        tok = res.result.token
+    })
+
+    it('affiche tous les utilisateurs', async () => {
+        const res = await server.inject({
+            method: 'get',
+            url: 'localhost:3000/users',
+        })
+    chai.expect(res.statusCode).to.equal(200);
+    //vérifie que le résultat est bien un tableau
+    chai.expect(Array.isArray(res.result)).to.equal(true);
+    chai.expect(res.result[0].login).to.equal('test');
+    chai.expect(res.result[0].password).to.equal('pass');
+    })
+    
+    it('affiche un utilisateur grâce à son token', async () => {
+        const res = await server.inject({
+            method: 'get',
+            url: 'localhost:3000/users/'+tok,
+        })
+    chai.expect(res.statusCode).to.equal(200);
+    chai.expect(res.result.login).to.equal('test');
+    chai.expect(res.result.password).to.equal('pass');
+    })
+
+    
+
+
 })
 
 describe("default route", () => {
