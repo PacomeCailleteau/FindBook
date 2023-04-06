5e4630ebe8bb4eadba60a46c76d878b6f1453524
Nicolas CHUSSEAU <nicolas.chusseau@etu.univ-nantes.fr>
2023-04-06 10:18:56 +0000

    la pi qui

--- server/dao/statDao.mjs
+++ server/dao/statDao.mjs
@@ -4,7 +4,7 @@ import {statModel} from "../model/statModel.mjs";
 
 // on crée une constante qui contient l'url de l'api
 const url = "https://serpapi.com/search.json?engine=google_trends&data_type=TIMESERIES&cat=22&date=today%201-m&tz=60"
-const apiKey = "&api_key=32" // b38f44324e616859286793b81a886039c76186119d3c93c2ab86690a90388b87
+const apiKey = "&api_key=b38f44324e616859286793b81a886039c76186119d3c93c2ab86690a90388b87"
 const apiUrl = url + apiKey + "&q="
 
 
